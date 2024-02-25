5af4fcc8515705c1e7778f2fd5451b7be47fec7b
Mykola Zhukovskyi <73184213+nikozhukovskyi@users.noreply.github.com>
2024-02-25 20:16:26 +0200

    Update First run.ps1

--- First run.ps1
+++ First run.ps1
@@ -2,6 +2,6 @@ Install-Module -Name PSWindowsUpdate
 Set-ExecutionPolicy –ExecutionPolicy RemoteSigned -force
 Reset-WUComponents –verbose
 Get-WUList
-Install-WindowsUpdate -AcceptAll -Install -IgnoreReboot | Out-File "C:\$(get-date -f dd-MM-yyyy_HH-mm)-WindowsUpdate.log" -force
+Install-WindowsUpdate -AcceptAll -Install -IgnoreReboot | Out-File "C:\$(get-date -f dd-MM-yyyy_HH-mm)-WindowsUpdate.log" -force | Format-table -autosize
 msg * save file to -> C:\ * WindowsUpdate.log 
 msg * Update.ps1  ->  C:\WimUpdateLog\  -> create dir! --> C:\WimUpdateLog
